1b298b0c93f3f16e284f179d97a6801bf3e5a609
vvcephei <vvceph@gmail.com>
2012-04-24 21:17:20 -0500

    still need accuracy on exp list

--- client/code/app/mainPage.js
+++ client/code/app/mainPage.js
@@ -2,16 +2,30 @@ var experiments = {}
   ;
 
 function refresh_experiments(experiments) {
-  var html = ss.tmpl['experiment-listItem'].render({
-    name: 'banana'
-  });
-  console.log(html);
+  var i=0
+    , j=0
+    , htmlExp
+    , htmlTrials
+    , htmlTrial
+    ;
+  for(i=0; i<experiments.length; i++) {
+    htmlExp = ss.tmpl['experiment-listItem'].render({
+      name: experiments[i].name
+    });
+    htmlTrials = $("<ul class=\"nav nav-list\">");
+    for(j=0; j<experiments[i].results.length; j++) {
+      htmlTrial = ss.tmpl['experiment-listItemTrial'].render({
+        id: experiments[i].results[j].id
+      });
+      htmlTrials = $(htmlTrials).append(htmlTrial);
+    }
+    htmlExp = $(htmlExp).append(htmlTrials);
+    $(htmlExp).appendTo("#experimentsList");
+  }
 }
 window.r = refresh_experiments;
-console.log('reached');
 
 ss.rpc('list.experiments',function(res){
   experiments = res
   refresh_experiments(res);
-console.log('reached');
 });
